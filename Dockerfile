4f3611d59c7a67604ce35133d18735f607cbd449
Rafael De Pieri Barbosa <s2it_rbarbosa@uolinc.com>
2018-12-27 12:41:05 -0200

    Update initialize-sh file

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ ADD build/libs/*.jar app.jar
 
 RUN bash -c 'touch /app.jar'
 
-ENTRYPOINT ["java","-Dspring.profiles.active=production","-jar", "app.jar"]
+ENTRYPOINT ["java","-jar", "app.jar"]
--- initialize.sh
+++ initialize.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+echo "============================="
 echo "Generating app artifactory..."
+echo "============================="
     ./gradlew clean build
 
+echo "============================"
 echo "Building app docker image..."
+echo "============================"
     docker build -t playlist-by-weather .
 
+echo "============================"
 echo "Deploying the application..."
+echo "============================"
     docker-compose up
